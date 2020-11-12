6a6165e7ae9cbbb426a7821351e53f207c526a6a
Rove Kosmos <37148727+Rove-Kosmos@users.noreply.github.com>
2020-11-12 13:52:46 +0100

    Initial commit

---
+++ src/qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5fad2fd179530b001fd25cbb",
+				"5fad2fd179530b001fd25cbd"
+			]
+		},
+		"5fad2fd179530b001fd25cbb": {
+			"id": "5fad2fd179530b001fd25cbb",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5fad2fd179530b001fd25cbd": {
+			"id": "5fad2fd179530b001fd25cbd",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app"
+}
